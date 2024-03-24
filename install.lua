dea0a832f7e87c29ccb180c477a3f10b3dcd902d
akki697222 <newakki69@outlook.jp>
2024-03-24 14:49:36 +0900

    installer

--- install.lua
+++ install.lua
@@ -1,37 +1,84 @@
-local ml_dirs ={
-    "bin",
-    "boot",
-    "data",
-    "etc",
-    "home",
-    "lib",
-    "proc",
+local version = "1.00"
+local mltext = "MoonLight Linux version " .. version
+local tx, ty = term.getSize()
+local s = 1
+local bootpath = {
+    "/moonlight/install.lua",
     "shell",
-    "sys",
-    "tmp",
-    "var"
+    "shutdown"
+}
+local options = {
+    "Install Moonlight Linux",
+    "CraftOS",
+    "Exit"
 }
 
+local function centerText(text)
+    local textLength = string.len(text)
+    local centerX = math.floor((tx - textLength) / 2)
+    return centerX
+end
+
 local function mkdir(n)
-    shell.run("mkdir "..n)
+    shell.run("mkdir " .. n)
 end
 
 local function wget(url)
-    shell.run("wget "..url)
+    shell.run("wget " .. url)
 end
 
 local function cd(path)
-    shell.run("cd "..path)
+    shell.run("cd " .. path)
+end
+
+local function drawBoxEvent()
+    term.clear()
+    term.setTextColor(colors.lightBlue)
+    term.setCursorPos(centerText(mltext), 2)
+    io.write(mltext)
+    term.setTextColor(colors.white)
+    term.setCursorPos(3, ty - ty + 5)
+    for i = 1, #options do
+        if i == s then
+            term.clearLine()
+            io.write("> ")
+            io.write(options[i])
+            term.setCursorPos(3, ty - ty + 5 + i)
+        else
+            term.clearLine()
+            io.write("  ")
+            io.write(options[i])
+            term.setCursorPos(3, ty - ty + 5 + i)
+        end
+        term.setBackgroundColor(colors.black)
+        term.setTextColor(colors.white)
+    end
+    term.setBackgroundColor(colors.black)
+    term.setTextColor(colors.white)
+    while true do
+        local event, key = os.pullEvent("key")
+        if key == keys.up and s > 1 then
+            s = s - 1
+            drawBoxEvent()
+        elseif key == keys.down and s < #options then
+            s = s + 1
+            drawBoxEvent()
+        elseif key == keys.enter then
+            term.clear()
+            term.setCursorPos(1, 1)
+            shell.run(bootpath[s])
+            shell.exit()
+            return
+        end
+    end
 end
 
 local function install()
     mkdir("moonlight")
     cd("moonlight")
-    for index, value in ipairs(ml_dirs) do
-        mkdir(value)
-    end
-    cd("boot")
-    mkdir("mlbtl")
-    cd("mlbtl")
-    wget("https://raw.githubusercontent.com/akki697222/Moonlight-CC-T-Linux-Like-OS/main/moonlight/boot/mlbtl/loader.lua")
+    wget("https://raw.githubusercontent.com/akki697222/Moonlight-CC-T-Linux-Like-OS/main/moonlight/install.lua")
+    term.clear()
+    drawBoxEvent()
 end
+
+install()
---
+++ moonlight/install.lua
@@ -0,0 +1,82 @@
+local function createDirectories(maindir, subdir)
+    for _, dir in ipairs(maindir) do
+        fs.makeDir(dir)
+        print("Created directory: "..dir)
+    end
+    
+    for main, subdirs in pairs(subdir) do
+        for _, subdir in ipairs(subdirs) do
+            local path = fs.combine(main, subdir)
+            fs.makeDir(path)
+            print("Created directory: "..path)
+        end
+    end
+end
+
+local function wget(url)
+    shell.run("wget https://raw.githubusercontent.com/akki697222/Moonlight-CC-T-Linux-Like-OS/main/moonlight/"..url..".lua")
+end
+
+local function cd(path)
+    shell.run("cd " .. path)
+end
+
+local function gettingpackage()
+    local bin = {"libdevmodehandler", "liblogerror", "liblogging", "libsyslog", "libshellstat", "libroothandler", "libpasswordhandler", "libuserhandler", "properprint"}
+    local shell = {"cash/cash", "cash/env"}
+    local sys = {"kernel/boot", "kernel/kernel"}
+    local proc = {"clabel","currentuser","defaultuser","devmode","pass","root","shellrunning"}
+    cd("bin")
+    for _, wget in ipairs(bin) do
+        wget(wget..".lua")
+    end
+    cd("/moonlight")
+    cd("boot")
+    cd("mlbtl")
+    wget("https://raw.githubusercontent.com/akki697222/Moonlight-CC-T-Linux-Like-OS/main/moonlight/boot/mlbtl/loader")
+    cd("/moonlight")
+    cd("shell")
+    cd("cash")
+    for _, wget in ipairs(shell) do
+        wget(wget..".lua")
+    end
+    cd("/moonlight/shell")
+    cd("moonshell")
+    wget("https://raw.githubusercontent.com/akki697222/Moonlight-CC-T-Linux-Like-OS/main/moonlight/moonshell/shell")
+    cd("/moonlight")
+    cd("sys")
+    cd("kernel")
+    for _, wget in ipairs(sys) do
+        wget(wget..".lua")
+    end
+    cd("/moonlight")
+    cd("/proc")
+    for _, v in ipairs(proc) do
+        local handle = fs.open("/moonlight/proc/"..v, "w")
+        handle.close()
+    end
+    wget("https://raw.githubusercontent.com/akki697222/Moonlight-CC-T-Linux-Like-OS/main/moonlight/proc/vinfo")
+end
+
+local mdir = {
+    "bin",
+    "boot",
+    "data",
+    "etc",
+    "home",
+    "lib",
+    "proc",
+    "shell",
+    "sys",
+    "tmp",
+    "var",
+}
+local sdir = {
+    boot = {"mlbtl"},
+    shell = {"cash", "moonshell"},
+    sys = {"firmware", "kernel"},
+    var = {"logs"},
+}
+
+createDirectories(mdir, sdir)
+gettingpackage()
